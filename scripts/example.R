d7e26a832f63c0df5672be1484c9b7c3262c4017
Wouter Saelens <wouter.saelens@gmail.com>
2017-08-23 11:22:30 +0200

    updated example to reorder

--- scripts/example.R
+++ scripts/example.R
@@ -1,8 +1,11 @@
+library(tidyverse)
+
 toys <- readRDS("scripts/toys.rds")
 
-toy_id <- which(toys$perturbator_id == "hairy_small")[[1]]
+#toy_id <- which(toys$perturbator_id == "hairy")[[1]]
+toy_id <- which(toys$perturbator_id == "hairy_large")[[4]]
 
 task1 <- toys$gs[[toy_id]]
 task2 <- toys$toy[[toy_id]]
 
-plot_strip_connections(task1, task2)
+plot_strip_connections(task1, task2, reorder = TRUE)
