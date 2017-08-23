aa44a54f4975d0a15cb0f42063056cb5ca5f9f74
Wouter Saelens <wouter.saelens@gmail.com>
2017-08-23 11:22:05 +0200

    add order optimization

--- R/plot_strip.R
+++ R/plot_strip.R
@@ -1,28 +1,38 @@
 #' @import dplyr ggplot2 purrr
 #' @export
-plot_strip <- function(task1, task2, margin=0.2) {
-  task1$milestone_network <- task1$milestone_network %>%
+plot_strip <- function(task1, task2, margin=0.1, reorder = TRUE) {
+  if (reorder) {
+    task1$milestone_network <- optimize_order(task1$milestone_network)
+    task2$milestone_network <- optimize_order(task2$milestone_network)
+  }
+
+  milestone_network1 <- task1$milestone_network %>%
     mutate(
       cumstart = c(0, cumsum(length)[-length(length)]) + margin * (seq_len(n())-1),
       cumend = c(cumsum(length)) + margin * (seq_len(n())-1)
     )
-  task2$milestone_network <- task2$milestone_network %>%
+  milestone_network2 <- task2$milestone_network %>%
     mutate(
       cumstart = c(0, cumsum(length)[-length(length)]) + margin * (seq_len(n())-1),
       cumend = c(cumsum(length)) + margin * (seq_len(n())-1)
     )
 
-  prog1 <- task1$progression %>% left_join(task1$milestone_network, by=c("from", "to")) %>% mutate(cumpercentage=percentage*length + cumstart) %>% rename_at(vars(-cell_id), ~paste0(., 1))
-  prog2 <- task2$progression %>% left_join(task2$milestone_network, by=c("from", "to")) %>% mutate(cumpercentage=percentage*length + cumstart) %>% rename_at(vars(-cell_id), ~paste0(., 2))
+  prog1 <- task1$progression %>% left_join(milestone_network1, by=c("from", "to")) %>% mutate(cumpercentage=percentage*length + cumstart) %>% rename_at(vars(-cell_id), ~paste0(., 1))
+  prog2 <- task2$progression %>% left_join(milestone_network2, by=c("from", "to")) %>% mutate(cumpercentage=percentage*length + cumstart) %>% rename_at(vars(-cell_id), ~paste0(., 2))
 
   prog <- full_join(prog1, prog2, by=c("cell_id"))
 
+  ymax <- max(milestone_network2$cumend)
+  xmax <- max(milestone_network1$cumend)
+
   ggplot(prog) +
+    geom_rect(aes(xmin=cumstart, xmax=cumend, ymin=0, ymax=ymax), data=milestone_network1, alpha=0.1) +
+    geom_rect(aes(ymin=cumstart, ymax=cumend, xmin=0, xmax=xmax), data=milestone_network2, alpha=0.1) +
     geom_point(aes(cumpercentage1, cumpercentage2)) +
-    geom_vline(aes(xintercept=cumstart), data=task1$milestone_network) +
-    geom_vline(aes(xintercept=cumend), data=task1$milestone_network, linetype="dashed") +
-    geom_hline(aes(yintercept=cumstart), data=task2$milestone_network) +
-    geom_hline(aes(yintercept=cumend), data=task2$milestone_network, linetype="dashed") +
+    geom_vline(aes(xintercept=cumstart), data=milestone_network1, alpha=0.5) +
+    geom_vline(aes(xintercept=cumend), data=milestone_network1, linetype="dashed", alpha=0.5) +
+    geom_hline(aes(yintercept=cumstart), data=milestone_network2, alpha=0.5) +
+    geom_hline(aes(yintercept=cumend), data=milestone_network2, linetype="dashed", alpha=0.5) +
     ggtitle(paste0(task1$id, " -> ", task2$id)) +
     theme_clean()
 }
--- R/plot_strip_connections.R
+++ R/plot_strip_connections.R
@@ -1,15 +1,17 @@
 #' @import dplyr ggplot2 purrr
 #' @export
-plot_strip_connections <- function(task1, task2) {
-  # make sure the order of the milestone_networks stay the same between the connection plots and the strip plots, therefore we already sort them here
-  task1$milestone_network <- task1$milestone_network %>% arrange(from, to)
-  task2$milestone_network <- task2$milestone_network %>% arrange(from, to)
+plot_strip_connections <- function(task1, task2, reorder=TRUE, margin=0.1) {
+  if (reorder) {
+    # make sure the order of the milestone_networks stay the same between the connection plots and the strip plots, therefore we already sort them here
+    task1$milestone_network <- optimize_order(task1$milestone_network)
+    task2$milestone_network <- optimize_order(task2$milestone_network)
+  }
 
   empty_max <- function(x) {if(length(x) > 0) {max(x)} else {0}}
 
-  strip_plot <- plot_strip(task1, task2)
-  connections_plotdata1 <- make_connection_plotdata(task1$milestone_network)
-  connections_plotdata2 <- make_connection_plotdata(task2$milestone_network)
+  strip_plot <- plot_strip(task1, task2, reorder = FALSE, margin=margin)
+  connections_plotdata1 <- make_connection_plotdata(task1$milestone_network, margin=margin)
+  connections_plotdata2 <- make_connection_plotdata(task2$milestone_network, margin=margin)
 
   connections_plot1 <- plot_connections(task1$milestone_network, orientation = -1, plotdata=connections_plotdata1)
   connections_plot2 <- plot_connections(task2$milestone_network, orientation = -1, plotdata=connections_plotdata2) + coord_flip()
