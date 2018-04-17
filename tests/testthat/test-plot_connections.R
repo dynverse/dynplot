4d3dce42368695219ad61ca6558b2539573407d7
Wouter Saelens <wouter.saelens@gmail.com>
2018-04-17 16:50:35 +0200

    add test for plot connections

---
+++ tests/testthat/test-plot_connections.R
@@ -0,0 +1,18 @@
+context("Test plot_combined")
+
+data("toy_tasks", package="dyntoy")
+
+toy_tasks <- toy_tasks %>% group_by(toy_tasks$trajectory_type) %>% filter(row_number() == 1) %>% ungroup()
+
+for (taski in seq_len(nrow(toy_tasks))) {
+  task <- extract_row_to_list(toy_tasks, taski)
+
+  test_that(paste0("Perform dimred on trajectory with task ", task$id), {
+    g <- plot_connections(task$milestone_network)
+    expect_is(g, "ggplot")
+
+    pdf("/dev/null")
+    print(g)
+    dev.off()
+  })
+}
