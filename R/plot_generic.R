32bb958b98bb8d20d6af1da8b075ae5088f5f307
Wouter Saelens <wouter.saelens@gmail.com>
2018-04-27 12:36:45 +0200

    give message when using generic plotting

--- R/plot_generic.R
+++ R/plot_generic.R
@@ -1,9 +1,11 @@
 #' @export
 `plot.dynwrap::with_expression` <- function(x, ...) {
+  message("Using plot_dimred")
   plot_dimred(x)
 }
 
 #' @export
 `plot.dynwrap::with_trajectory` <- function(x, ...) {
+  message("Using plot_graph")
   plot_graph(x)
 }
