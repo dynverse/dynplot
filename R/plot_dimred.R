f7fb0ea979cba7fbdff15cccb1472f5674b82944
zouter <wouter.saelens@gmail.com>
2018-05-27 22:28:20 +0200

    add more docs

--- R/plot_dimred.R
+++ R/plot_dimred.R
@@ -1,8 +1,10 @@
 #' Plot the trajectory on dimensionality reduction
 #'
+#'
+#'
 #' @param expression_source Source of the expression
-#' @param plot_milestone_network Whether to plot the milestone network
-#' @param plot_trajectory Whether to plot the trajectory
+#' @param plot_milestone_network Whether to plot the projected milestone network on the dimensionality reduction
+#' @param plot_trajectory Whether to plot the projected trajectory on the dimensionality reduction
 #'
 #' @inheritParams add_cell_coloring
 #' @inheritParams add_milestone_coloring
