bbb72e9218c99b4fba72fb0268bbc039b9aada51
Robrecht Cannoodt <brechtus@gmail.com>
2018-06-05 13:31:32 +0200

    allow add_milestone_colouring to be passed the milestone_ids

--- R/add_coloring.R
+++ R/add_coloring.R
@@ -1,27 +1,32 @@
 #' Add milestone coloring
 #' @param color_milestones How to color the cells
 #' @param milestones Tibble containing the `milestone_id` and a `color` for each milestone
+#'
+#' @include milestone_palette.R
 add_milestone_coloring <- function(
-  milestones=NULL,
-  color_milestones=c("auto", "given")
+  milestones = NULL,
+  color_milestones = c("auto", "given", get_milestone_palette_names())
 ) {
-  color_milestones <- match.arg(color_milestones[1], c("auto", "given", dynplot::get_milestone_palette_names()))
+  color_milestones <- match.arg(color_milestones)
+
+  if (!is.data.frame(milestones)) {
+    milestones <- data_frame(milestone_id = milestones)
+  }
 
   if(color_milestones == "given") {
-    if(!"color" %in% names(milestones)) {stop("Milestone colors need to be given")}
-  } else if (color_milestones == "auto") {
     if(!"color" %in% names(milestones)) {
-      milestones <- milestones %>%
-        mutate(color = milestone_palette("Set3", n = n()))
+      stop("Milestone colors need to be given")
     }
   } else if (color_milestones %in% get_milestone_palette_names()) {
-    milestones <- milestones %>%
-      mutate(color = milestone_palette(color_milestones, n = n()))
+    if (!(color_milestones == "auto" && "color" %in% names(milestones))) {
+      milestones <- milestones %>%
+        mutate(color = milestone_palette(color_milestones, n = n()))
+    }
   }
 
   milestones
 }
-
+formals(add_milestone_coloring)$color_milestones <- unique(c("auto", "given", get_milestone_palette_names()))
 
 
 #' Add coloring
