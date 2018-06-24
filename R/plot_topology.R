db1149a9eff674c66fb6e5b09b596fdc6aa33f07
zouter <wouter.saelens@gmail.com>
2018-06-24 23:34:00 +0200

    automatically determine layout of topology

--- R/plot_topology.R
+++ R/plot_topology.R
@@ -2,16 +2,31 @@
 #'
 #' @inheritParams add_cell_coloring
 #' @inheritParams add_milestone_coloring
+#' @inheritParams ggraph::ggraph
 #' @export
 plot_topology <- dynutils::inherit_default_params(
   list(add_milestone_coloring),
   function(
   traj,
   color_milestones,
-  milestones
+  milestones,
+  layout = NULL
   ) {
     # make sure a trajectory was provided
     testthat::expect_true(dynwrap::is_wrapper_with_trajectory(traj))
+
+    # determine optimal layout
+    gr <- model$milestone_network %>% igraph::graph_from_data_frame()
+    if (igraph::girth(gr)$girth > 0) {
+      # cyclic
+      layout <- "kk"
+    } else {
+      # tree
+      if(is.null(traj$root_milestone_id)) {
+        traj <- traj %>% add_root()
+      }
+      layout <- "tree"
+    }
 
     milestone_graph <- as_tbl_graph(traj$milestone_network)
     milestone_positions <- milestone_graph %>%
