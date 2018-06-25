0bd3c3fe5894a2aeeed61d3339d70ed9028185e9
zouter <wouter.saelens@gmail.com>
2018-06-25 19:22:15 +0200

    also test disconnected tasks with plot_topology

--- tests/testthat/test-plot_topology.R
+++ tests/testthat/test-plot_topology.R
@@ -1,6 +1,6 @@
 context("Test plot_topology")
 
-test_tasks(load_test_tasks("toy_tasks_connected"), function(task) {
+test_tasks(load_test_tasks("toy_tasks"), function(task) {
   test_that(paste0("plot_topology on ", task$id), {
     g <- plot_topology(task)
     expect_ggplot(g)
