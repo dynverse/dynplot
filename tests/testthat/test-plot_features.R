5f9f6a439650009c4724759c80bb1a9b06777ae6
Robrecht Cannoodt <rcannood@gmail.com>
2019-01-04 12:41:27 +0100

    remove plot_features function

--- tests/testthat/test-plot_features.R
+++
@@ -1,8 +0,0 @@
-context("Test plot_features")
-
-test_datasets(load_test_datasets("toy_datasets_connected"), function(dataset) {
-  test_that(paste0("Plotting features of ", dataset$id), {
-    g <- plot_features(dataset)
-    expect_ggplot(g)
-  })
-})
