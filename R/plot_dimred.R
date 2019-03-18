48777410d356ce6cdb709e44c7ad9f4417f611c9
Wouter Saelens <wouter.saelens@gmail.com>
2019-03-18 14:07:10 +0100

    add more dimred examples

--- R/plot_dimred.R
+++ R/plot_dimred.R
@@ -102,6 +102,10 @@ project_waypoints <- function(
 #' dataset <- dyntoy::generate_dataset(model = "bifurcating", num_cells = 500)
 #' plot_dimred(dataset)
 #' plot_dimred(dataset, dimred = dyndimred::dimred_umap)
+#'
+#' dimred <- dyndimred::dimred_dm_destiny(dataset$expression)
+#' plot_dimred(dataset, dimred = dimred)
+#'
 #' plot_dimred(dataset, color_cells = "pseudotime")
 #' plot_dimred(dataset, color_density = "grouping", grouping = dynwrap::group_onto_nearest_milestones(dataset))
 #'
