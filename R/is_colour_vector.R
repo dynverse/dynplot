1abe642d53aed8795dbaa7f4c0dae6ac62090f0d
Robrecht Cannoodt <rcannood@gmail.com>
2019-04-17 11:50:41 +0200

    daaaa-aag!

--- R/is_colour_vector.R
+++
@@ -1,6 +0,0 @@
-#' @importFrom grDevices colours
-is_colour_vector <- function(col) {
-  all(col %in% grDevices::colours() | grepl("^#[0-9a-fA-F]{6}$|^#[0-9a-fA-F]{8}$", col))
-}
-
-is_color_vector <- is_colour_vector
