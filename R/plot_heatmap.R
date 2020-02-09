0abbe2469aecb898cdbcea83b36c2b37f95469e0
Wouter Saelens <wouter.saelens@gmail.com>
2020-02-09 06:35:38 +0100

    rename x2 to x

--- R/plot_heatmap.R
+++ R/plot_heatmap.R
@@ -1,20 +1,11 @@
 #' Plot the expression across a trajectory in a heatmap
 #'
-#' When using RStudio: the heatmap will not show inside the plot area, but will be visible once you click the zoom button.
-#'
 #' @param features_oi The features of interest, either the number of features or a vector giving the names of the different features
 #' @param clust The method to cluster the features, or a hclust object
-#' @param cell_feature_importances The importances of every feature in every cell, as returned by [dynfeature::calculate_cell_feature_importance()]
-#' @param heatmap_type The type of heatmap, either tiled or dotted
 #' @param scale Whether to rescale the expression, can be a function or boolean
-#'
-#' @inheritParams plot_onedim
+#' @param features_labels The labels for the features. If NULL, will use either the names of the features_oi (if exists) or the features_oi themselves. If not NULL, this should be a named character vector providing the label of a features given its feature_id
 #'
 #' @keywords plot_trajectory
-#'
-#' @import tidygraph
-#' @import ggraph
-#' @importFrom patchwork wrap_plots
 #'
 #' @examples
 #' data(example_linear)
@@ -23,185 +14,237 @@
 #' data(example_bifurcating)
 #' plot_heatmap(example_bifurcating)
 #'
+#' @include plot_heatmap_annotations.R plot_heatmap_annotations_velocity.R plot_heatmap_annotations_features.R
+#'
 #' @export
-plot_heatmap_old <- function(
-  trajectory,
-  expression_source = "expression",
-  features_oi = 20,
-  clust = "ward.D2",
-  margin = 0.02,
-  color_cells = NULL,
-  milestones = NULL,
-  milestone_percentages = trajectory$milestone_percentages,
-  grouping = NULL,
-  groups = NULL,
-  cell_feature_importances = NULL,
-  heatmap_type = c("tiled", "dotted"),
-  scale = dynutils::scale_quantile,
-  label_milestones = TRUE
+plot_heatmap <- inherit_default_params(
+  list(
+    annotate_milestone_percentages,
+    annotate_milestone_network,
+    annotate_velocity,
+    annotate_features,
+    add_milestone_coloring
+  ),
+  function(
+    dataset,
+    trajectory = dataset,
+    expression_source = dataset,
+
+    # feature parameters
+    features_oi = 20,
+    feature_info = NULL,
+    highlight_features,
+    row_gap = unit(3, "mm"),
+
+    clust = "ward.D2",
+    color_cells = NULL,
+
+    # milestones
+    milestones = NULL,
+    milestone_percentages = trajectory$milestone_percentages,
+    color_milestones,
+
+    grouping = NULL,
+    groups = NULL,
+    scale = dynutils::scale_quantile,
+
+    # cell (column) parameters
+    column_gap,
+    top_annotation = list(),
+
+    # milestone percentages
+    plot_milestone_percentages,
+
+    # milestone network
+    plot_milestone_network,
+    plot_milestones,
+    milestone_network_orientation,
+    milestone_network_arrow,
+
+    # velocity
+    plot_velocity,
+    velocity_each,
+
+    # legends
+    heatmap_legend_param = list(direction = "horizontal")
 ) {
+  requireNamespace("ComplexHeatmap")
+
   # make sure a trajectory was provided
   testthat::expect_true(dynwrap::is_wrapper_with_trajectory(trajectory))
 
-  # process expression
-  expression <- get_expression(trajectory, expression_source)
-
-  # convert to regular matrix if sparse
-  if (dynutils::is_sparse(expression)) {
-    expression <- as.matrix(expression)
-  }
-
-  if (is.function(scale)) {
-    expression <- scale(expression)
-  } else if (is.logical(scale) && scale) {
-    expression <- dynutils::scale_quantile(expression)
-  }
-
-  # check milestones, make sure it's a data_frame
-  milestones <- check_milestones(trajectory, milestones)
-
-  # get features oi
-  features_oi <- check_features_oi(trajectory, expression, features_oi, cell_feature_importances)
-  expression <- expression[, features_oi]
-
-  # cluster features
-  if (is.character(clust)) {
-    clust <- hclust(as.dist(correlation_distance(t(expression))), method = clust)
-  }
-  feature_order <- colnames(expression)[clust$order]
-
-  # put cells on one edge with equal width per cell
+  # linearize cells
   linearised <- linearise_cells(
     trajectory = trajectory,
     equal_cell_width = TRUE,
-    margin = margin
+    margin = 0
   )
 
-  # melt expression
-  molten <- expression %>%
-    reshape2::melt(varnames = c("cell_id", "feature_id"), value.name = "expression") %>%
-    mutate_if(is.factor, as.character) %>%
-    mutate(feature_position = as.numeric(factor(feature_id, feature_order))) %>%
-    left_join(linearised$progressions, "cell_id")
+  # get expression
+  expression <- get_expression(dataset, expression_source = expression_source)
+  expression_matrix <- as.matrix(expression[linearised$progressions$cell_id, features_oi])
 
-  # check importances
-  if (!is.null(cell_feature_importances)) {
-    molten <- left_join(
-      molten,
-      cell_feature_importances,
-      c("cell_id", "feature_id")
+  # get features oi
+  features_oi <- check_features_oi(trajectory, expression, features_oi)
+
+  # cluster features
+  clust <- hclust(as.dist(dynutils::correlation_distance(t(expression_matrix))), method = "average")
+
+  cell_ids <- rownames(expression_matrix)
+
+  # create milestones tibble
+  milestones <- check_milestones(trajectory, milestones, check_color = TRUE, color_milestones = color_milestones)
+
+  # add milestone network labels based on milestone labels
+  linearised$milestone_network <- linearised$milestone_network %>%
+    left_join(milestones %>% select(from = milestone_id, label_from = label), "from") %>%
+    left_join(milestones %>% select(to = milestone_id, label_to = label), "to") %>%
+    mutate(
+      label = paste0(label_from, " \U2192 ", label_to)
     )
+  linearised$progressions <- linearised$progressions %>% left_join(
+    linearised$milestone_network %>% select(from, to, label),
+    c("from", "to")
+  ) %>% mutate(
+    label = factor(label, levels = linearised$milestone_network$label)
+  )
+
+  ## setup annotations
+  bottom_annotation <- list()
+  right_annotation <- list()
+  annotation_legends <- list()
+
+  # plot milestone network
+  if(first(plot_milestone_network) != "none") {
+    c(annotation_milestone_network) %<-% annotate_milestone_network(
+      dataset,
+      trajectory,
+      milestones,
+      linearised,
+      plot_milestone_network = plot_milestone_network,
+      milestone_network_orientation = milestone_network_orientation,
+      milestone_network_arrow = milestone_network_arrow,
+      plot_milestones = plot_milestones,
+      column_gap = column_gap
+    )
+
+    if (first(plot_milestone_network) == "top") {
+      top_annotation$`Milestone network` <- annotation_milestone_network
+    } else {
+      bottom_annotation$`Milestone network` <- annotation_milestone_network
+    }
   }
 
-  # plot heatmap
-  x_limits <- c(min(linearised$milestone_network$cumstart) - 1, max(linearised$milestone_network$cumend) + 1)
-  y_limits <- c(0.5, length(feature_order) + 0.5)
+  # plot velocity
+  if(first(plot_velocity) != "none") {
+    c(annotation_velocity, legend_velocity) %<-% annotate_velocity(
+      dataset,
+      trajectory,
+      linearised,
+      plot_velocity = plot_velocity,
+      velocity_each = velocity_each
+    )
 
-  heatmap <-
-    if (heatmap_type == "tiled") {
-      if (is.null(cell_feature_importances)) {
-        ggplot(molten) +
-          geom_tile(aes(cumpercentage, feature_position, fill = expression)) +
-          scale_fill_distiller(palette = "RdBu") +
-          scale_x_continuous(NULL, breaks = NULL, expand = c(0, 0), limits = x_limits) +
-          scale_y_continuous(NULL, expand = c(0, 0), breaks = seq_along(feature_order), labels = feature_order, position = "left", limits = y_limits) +
-          theme(legend.position = "none", plot.margin = margin(), plot.background = element_blank(), panel.background = element_blank())
-      } else {
-        ggplot(molten) +
-          # geom_tile(aes(cumpercentage, feature_position, alpha = importance), fill = "black") +
-          geom_rect(aes(xmin = cumpercentage-0.5, xmax = cumpercentage+0.5, ymin = feature_position+scale_minmax(importance)/10*5, ymax = feature_position-scale_minmax(importance)/10*5, fill = expression)) +
-          scale_fill_distiller(palette = "RdBu") +
-          scale_x_continuous(NULL, breaks = NULL, expand = c(0, 0), limits = x_limits) +
-          scale_y_continuous(NULL, expand = c(0, 0), breaks = seq_along(feature_order), labels = feature_order, position = "left", limits = y_limits) +
-          scale_alpha_continuous(range = c(0, 1)) +
-          theme(legend.position = "none", plot.margin = margin(), plot.background = element_blank(), panel.background = element_blank())
-      }
-    } else if (heatmap_type == "dotted") {
-      if (is.null(cell_feature_importances)) {
-        ggplot(molten) +
-          geom_point(aes(cumpercentage, feature_position, color = expression, size = expression)) +
-          scale_color_distiller(palette = "RdBu") +
-          scale_size_continuous(range = c(0, 6)) +
-          scale_x_continuous(NULL, breaks = NULL, expand = c(0, 0), limits = x_limits) +
-          scale_y_continuous(NULL, expand = c(0, 0), breaks = seq_along(feature_order), labels = feature_order, position = "left", limits = y_limits) +
-          theme(legend.position = "none", plot.margin = margin(), plot.background = element_blank(), panel.background = element_blank())
-      } else {
-        ggplot(molten) +
-          geom_point(aes(cumpercentage, feature_position, color = expression, size = importance**2)) +
-          scale_color_distiller(palette = "RdBu") +
-          scale_size_continuous(range = c(0, 6)) +
-          scale_x_continuous(NULL, breaks = NULL, expand = c(0, 0), limits = x_limits) +
-          scale_y_continuous(NULL, expand = c(0, 0), breaks = seq_along(feature_order), labels = feature_order, position = "left", limits = y_limits) +
-          theme(legend.position = "none", plot.margin = margin(), plot.background = element_blank(), panel.background = element_blank())
-      }
+    if (first(plot_velocity) == "top") {
+      top_annotation$`RNA velocity` <- annotation_velocity
+    } else {
+      bottom_annotation$`RNA velocity` <- annotation_velocity
     }
 
-  # plot one dim
-  onedim <- plot_onedim(
+    annotation_legends$`RNA velocity` <- legend_velocity
+  }
+
+  # plot milestone percentages
+  if(first(plot_milestone_percentages) != "none") {
+    c(annotation_milestone_percentages, legend_milestone_id) %<-% annotate_milestone_percentages(
+      dataset, trajectory, milestones,
+      linearised
+    )
+
+    if (first(plot_milestone_percentages) == "top") {
+      top_annotation$`Milestone percentages` <- annotation_milestone_percentages
+    } else {
+      bottom_annotation$`Milestone percentages` <- annotation_milestone_percentages
+    }
+
+    annotation_legends$Milestones <- legend_milestone_id
+  }
+
+  # feature labels and feature annotation
+  c(
+    annotation_features,
+    feature_labels,
+    show_row_names,
+    row_labels,
+    row_names_gp
+  ) %<-% annotate_features(
+    dataset,
     trajectory,
-    linearised = linearised,
-    orientation = -1,
-    quasirandom_width = 0,
-    margin = margin,
-    color_cells = color_cells,
-    grouping = grouping,
-    groups = groups,
-    milestone_percentages = milestone_percentages,
-    milestones = milestones,
-    plot_cells = FALSE,
-    label_milestones = label_milestones
-  ) +
-    scale_x_continuous(expand = c(0, 0), limits = x_limits) +
-    theme(plot.margin = margin())
-
-  # plot dendrogram
-  dendrogram <- ggraph::ggraph(as.dendrogram(clust), "dendrogram") +
-    ggraph::geom_edge_elbow() +
-    scale_x_continuous(limits = c(-0.5, length(feature_order)-0.5), expand = c(0, 0)) +
-    scale_y_reverse() +
-    coord_flip() +
-    theme_graph() +
-    theme(plot.margin = margin())
-
-  # plot cell information
-  # TODO: Allow multiple cell info here, even "external" which does not fit into grouping,  milestone_percentages or pseudotime. The current solution is only temporary and ugly!
-  if (!is.null(grouping)) {
-    cell_annotation_positions <-
-      linearised$progressions %>%
-      add_cell_coloring(
-        color_cells = "grouping",
-        grouping = grouping,
-        trajectory = trajectory,
-        groups = groups,
-        milestones = milestones
-      )
-  } else if (!is.null(milestone_percentages)) {
-    cell_annotation_positions <-
-      linearised$progressions %>%
-      add_cell_coloring(
-        color_cells = "milestone",
-        milestone_percentages = milestone_percentages,
-        trajectory = trajectory,
-        milestones = milestones
-        )
-    }
-
-  cell_annotation <- ggplot(cell_annotation_positions$cell_positions) +
-    geom_point(aes(cumpercentage, 1, color = color)) +
-    cell_annotation_positions$color_scale +
-    scale_x_continuous(expand = c(0, 0), limits = x_limits) +
-    theme_graph() +
-    theme(legend.position = "top")
-
-  patchwork::wrap_plots(
-    empty_plot(),
-    cell_annotation,
-    dendrogram,
-    heatmap,
-    empty_plot(),
-    onedim,
-    ncol = 2,
-    widths = c(2, 10),
-    heights = c(0.5, 10, 2)
+    features_oi = features_oi,
+    feature_info = feature_info,
+    highlight_features = highlight_features
   )
-}
+
+  if(!is.null(annotation_features)) {
+    right_annotation$Features <- annotation_features
+  }
+
+  # wrap up annotations
+  top_annotation <- if(length(top_annotation) > 0) {
+    invoke(ComplexHeatmap::HeatmapAnnotation, top_annotation, which = "column")
+  } else {NULL}
+  bottom_annotation <- if(length(bottom_annotation) > 0) {
+    invoke(ComplexHeatmap::HeatmapAnnotation, bottom_annotation, which = "column")
+  } else {NULL}
+  right_annotation <- if(length(right_annotation) > 0) {
+    invoke(ComplexHeatmap::HeatmapAnnotation, right_annotation, which = "row")
+  } else {NULL}
+
+  browser()
+
+  heatmap <- ComplexHeatmap::Heatmap(
+    Matrix::t(dynutils::scale_quantile(expression_matrix)),
+    name = "Expression",
+
+    cluster_rows = clust,
+    row_split = 4,
+
+    # rows
+    row_gap = row_gap,
+
+    # columns
+    column_split = linearised$progressions$label,
+    column_gap = column_gap,
+
+    # features
+    show_row_names = show_row_names,
+    row_labels = row_labels,
+    row_names_gp = row_names_gp,
+
+    right_annotation = right_annotation,
+    cluster_columns = FALSE,
+    show_column_names = FALSE,
+
+    # heatmap
+    col = rev(RColorBrewer::brewer.pal(10, "RdBu")[2:9]),
+
+    # annotation
+    top_annotation = top_annotation,
+    bottom_annotation = bottom_annotation,
+
+    # legend
+    heatmap_legend_param = heatmap_legend_param
+  )
+
+  heatmap_list <- ComplexHeatmap::HeatmapList()
+  heatmap_list <- ComplexHeatmap::add_heatmap(heatmap, heatmap_list)
+
+  heatmap <- ComplexHeatmap::make_layout(
+    heatmap_list,
+    annotation_legend_list = annotation_legends,
+    merge_legend = TRUE,
+    heatmap_legend_side = "bottom",
+    annotation_legend_side = "bottom"
+  )
+  heatmap
+})
