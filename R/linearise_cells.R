422a3dd81796b190537d4e3833dd54dad033cc80
Robrecht Cannoodt <rcannood@gmail.com>
2020-12-10 12:51:23 +0100

    BUG FIX `linearise_cells()`: Fix ordering issue when `equal_cell_width` is `TRUE`.

--- R/linearise_cells.R
+++ R/linearise_cells.R
@@ -5,6 +5,8 @@
 #' @param no_margin_between_linear Whether to add a margin only when
 #' @param one_edge If TRUE, assigns each cell to one edge only
 #' @param equal_cell_width if TRUE, will give each cell equal width
+#'
+#' @importFrom dplyr lead
 linearise_cells <- function(
   trajectory,
   margin = 0.05,
@@ -25,45 +27,53 @@ linearise_cells <- function(
 
   if (equal_cell_width) {
     progressions <- progressions %>%
-      group_by(from, to) %>%
-      mutate(percentage2 = percentage + runif(n(), 0, 1e-6)) %>% # randomly position cells at same position
-      mutate(percentage2 = (rank(percentage2)-1)/n())
+      group_by(.data$from, .data$to) %>%
+      mutate(percentage2 = .data$percentage + stats::runif(n(), 0, 1e-6)) %>% # randomly position cells at same position
+      mutate(percentage2 = (rank(.data$percentage2)-1)/n())
 
-    milestone_network <- progressions %>%
-      group_by(from, to) %>%
-      summarise(length = n()) %>%
-      right_join(milestone_network %>% select(-length), c("from", "to")) %>%
-      mutate(length = ifelse(is.na(length), 1e-6, length)) %>% # add length of edges with no cells
+    milestone_network <-
+      left_join(
+        milestone_network %>% select(-.data$length),
+        progressions %>%
+          group_by(.data$from, .data$to) %>%
+          summarise(length = n(), .groups = "drop"),
+        by = c("from", "to")
+      ) %>%
+      mutate(length = ifelse(is.na(.data$length), 1e-6, .data$length)) %>% # add length of edges with no cells
       ungroup()
   } else {
     progressions$percentage2 <- progressions$percentage
   }
 
-  margin <- sum(milestone_network$length) * margin
+  margin_mult <- sum(milestone_network$length) * margin
 
   if (no_margin_between_linear) {
     # add margin only if froms not directly connected, or if to is a forking milestone, or if to is a converging milestone
-    milestone_network$add_margin <- (milestone_network$to != lead(milestone_network$from, default = "")) |
-      (table(milestone_network$from)[milestone_network$to] > 1) |
-      (table(milestone_network$to)[milestone_network$to] > 1)
+    num_to <- table(c(milestone_network$to, trajectory$milestone_ids)) - 1
+    num_from <- table(c(milestone_network$from, trajectory$milestone_ids)) - 1
+    milestone_network$add_margin <- unname(
+      (milestone_network$to != lead(milestone_network$from, default = "")) |
+      (num_from[milestone_network$to] > 1) |
+      (num_to[milestone_network$to] > 1)
+    )
   } else {
     milestone_network$add_margin <- TRUE
   }
 
-  milestone_network$n_margins <- c(0, cumsum(milestone_network$add_margin)[-nrow(milestone_network)])
+  milestone_network$n_margins <- unname(c(0, cumsum(milestone_network$add_margin)[-nrow(milestone_network)]))
 
   milestone_network <- milestone_network %>%
     mutate(
-      cumstart = c(0, cumsum(length)[-n()]) + n_margins * margin,
-      cumend = cumstart + length,
+      cumstart = lag(cumsum(.data$length), default = 0) + .data$n_margins * margin_mult,
+      cumend = .data$cumstart + .data$length,
       edge_id = factor(seq_len(n()))
     )
 
   progressions <- progressions %>%
     left_join(milestone_network, by = c("from", "to")) %>%
-    mutate(cumpercentage = cumstart + percentage2 * length)
+    mutate(cumpercentage = .data$cumstart + .data$percentage2 * .data$length)
 
-  lst(milestone_network, progressions, margin)
+  lst(milestone_network, progressions, margin = margin_mult)
 }
 
 
